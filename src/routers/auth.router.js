17fd7490fc2358eccc4b691e736fda7129b9a1c6
Diego Alzate <dalzate@lean-tech.io>
2023-03-25 08:59:12 -0500

    Initial commit

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import * as AuthController from "../controllers/auth.controller.js";
-import { loginValidation } from "../middlewares/validations.js";
 
 const router = new Router();
 
-router.post("/login", loginValidation, AuthController.login);
+router.post("/login", AuthController.login);
 router.get("/logout", AuthController.logout);
 
 export default router;
